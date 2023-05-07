e4d833b892eeaa97c8bd0f87acba51db12166737
Iván Hidalgo <ivanhcenalmor@gmail.com>
2023-05-07 14:38:26 +0100

    Improved folder conversion

--- Image converter/image_converter.py
+++ Image converter/image_converter.py
@@ -8,8 +8,8 @@ def image_converter(filename, desired_extension):
     cv2.imwrite(file_name + desired_extension, img)
     return file_name + desired_extension
 
-def image_converter_from_folder(folder, desired_extension):
-    for filename in os.listdir(folder):
+def image_converter_from_folder(folder, desired_extension, input_extension):
+    for filename in [e for e in os.listdir(folder) if e.endswith(input_extension)]:
         image_converter(os.path.join(folder, filename), desired_extension)
         
 extension_choices = ['.jpg', '.png', '.jpeg', '.gif', '.bmp', '.tiff', '.tif', '.webp', '.svg', '.ico', '.jfif'] 
@@ -17,8 +17,8 @@ extension_choices = ['.jpg', '.png', '.jpeg', '.gif', '.bmp', '.tiff', '.tif', '
 # Define the window's contents
 layout = [[sg.Text('Your File'), sg.InputText('Default Filename', key='-Filename-'), sg.FileBrowse()],
           [sg.Text('Your Folder'), sg.InputText('Default Folder', key='-Folder-'), sg.FolderBrowse()],   
-          [sg.Text('Is a File'), sg.Checkbox('File', 1, key='-isFile-')],             
-          [sg.Text('Desired extension'), sg.OptionMenu(extension_choices, s=(15,2), key='-extension-')],
+          [sg.Text('Is a File'), sg.Checkbox('File', 1, key='-isFile-'), sg.Text('Given extension'), sg.OptionMenu(extension_choices, s=(15,2), key='-Input_extension-')],             
+          [sg.Text('Desired extension'), sg.OptionMenu(extension_choices, s=(15,2), key='-Extension-')],
           [sg.OK(), sg.Cancel()]]
 
 # Create the window
@@ -32,14 +32,14 @@ while True:
     elif event == "OK":
         if values['-isFile-']:
             if os.path.exists(values['-Filename-']):
-                new_filename = image_converter(values['-Filename-'], values['-extension-'])
+                new_filename = image_converter(values['-Filename-'], values['-Extension-'])
                 sg.popup('Image {} has been converted into {}.'.format(values['-Filename-'], new_filename))
             else:
                 raise ValueError('File does not exist.')
         else:
             if os.path.exists(values['-Folder-']):
-                image_converter_from_folder(values['-Folder-'], values['-extension-'])
-                sg.popup('All images in {} have been converted into extension {}.'.format(values['-Folder-'], values['-extension-']))
+                image_converter_from_folder(values['-Folder-'], values['-Extension-'], values['-Input_extension-'])
+                sg.popup('All images in {} have been converted into extension {}.'.format(values['-Folder-'], values['-Extension-']))
             else:
                 raise ValueError('Folder does not exist.')
 
