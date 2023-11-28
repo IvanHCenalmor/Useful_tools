c9b0540cd884f3750b98285cebca16f37a8b90c6
Iván Hidalgo <47150974+IvanHCenalmor@users.noreply.github.com>
2023-11-28 15:18:22 +0000

    Update trainingyt.py

--- trainingyt.py
+++ trainingyt.py
@@ -1,5 +1,12 @@
 import numpy as np
 import torch
+
+torch.cuda.is_available()
+torch.cuda.device_count()
+torch.cuda.current_device()
+torch.cuda.device(0) 
+torch.cuda.get_device_name(0)
+
 device = torch.device("cuda:0")
 for i in range(20):
     a[str(i)] = torch.tensor(list(np.random.random((100, 2500, 1000)).astype(np.float32)), device=device)
